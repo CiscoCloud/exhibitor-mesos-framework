61cdbaa1ed06c2461814a81c7c3c079b4cf7170e
serejja <serejja@gmail.com>
2016-04-26 12:13:49 +0300

    Add to ensemble with exactly same name as Exhibitor resolves itself to

--- src/main/scala/net/elodina/mesos/exhibitor/TaskConfig.scala
+++ src/main/scala/net/elodina/mesos/exhibitor/TaskConfig.scala
@@ -5,7 +5,8 @@ import play.api.libs.json._
 
 import scala.collection.mutable
 
-case class TaskConfig(exhibitorConfig: mutable.Map[String, String], sharedConfigOverride: mutable.Map[String, String], id: String, var hostname: String = "", var sharedConfigChangeBackoff: Long = 10000, var cpus: Double = 0.2, var mem: Double = 256, var ports: List[Util.Range] = Nil)
+case class TaskConfig(exhibitorConfig: mutable.Map[String, String], sharedConfigOverride: mutable.Map[String, String], id: String, var hostname: String = "",
+                      var exhibitorHostname: String = "", var sharedConfigChangeBackoff: Long = 10000, var cpus: Double = 0.2, var mem: Double = 256, var ports: List[Util.Range] = Nil)
 
 object TaskConfig {
   implicit val reader = (
@@ -13,6 +14,7 @@ object TaskConfig {
       (__ \ 'sharedConfigOverride).read[Map[String, String]].map(m => mutable.Map(m.toSeq: _*)) and
       (__ \ 'id).read[String] and
       (__ \ 'hostname).read[String] and
+      (__ \ 'exhibitorHostname).read[String] and
       (__ \ 'sharedConfigChangeBackoff).read[Long] and
       (__ \ 'cpu).read[Double] and
       (__ \ 'mem).read[Double] and
@@ -25,6 +27,7 @@ object TaskConfig {
         "sharedConfigOverride" -> tc.sharedConfigOverride.toMap[String, String],
         "id" -> tc.id,
         "hostname" -> tc.hostname,
+        "exhibitorHostname" -> tc.exhibitorHostname,
         "cpu" -> tc.cpus,
         "mem" -> tc.mem,
         "sharedConfigChangeBackoff" -> tc.sharedConfigChangeBackoff,
